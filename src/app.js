c6d7f1af9f4b2d96a2af1700f8547595308121dc
LUCAS\monto <lucasmontoby@gmail.com>
2024-04-23 19:53:29 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express =  require('express');
+const path = require("path");
+const app = express();
+const methodOverride  = require("method-override");
+
+app.set('views', path.resolve(__dirname, './views'));
+app.set( "view engine", "ejs" );
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(express.urlencoded({extended:false}));
+app.use(methodOverride("_method"));
+
+app.listen(3001 , ()=>{
+    console.log("Server is running on port 3001")
+});
---
+++ src/database/models/Movie.js
@@ -0,0 +1,9 @@
+module.exports = (sequelize, DataTypes) => {
+    let alias = "Movie";
+    let cols  = {
+
+}
+    let config = {
+
+    }
+};
